c5171ebb67b7a73898827973cfdce83dc0c15223
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-12-02 18:00:39 -0500

    completed advent of code day 2 part 2

--- day2.ts
+++ day2.ts
@@ -22,4 +22,36 @@ function dive(){
     return;
 }
 
+function dive2() {
+    let input = fs.readFileSync('./day2.txt', 'utf-8').trim().split(/\s|\n/);
+    let final;
+    let depth = 0;
+    let aim = 0;
+    let horizontal = 0;
+    for (let i = 0; i < input.length; i++) {
+        if (input[i] === 'forward') {
+            horizontal += parseInt(input[i + 1]);
+            depth += parseInt(input[i + 1]) * aim; 
+        } else if (input[i] === 'down') {
+            aim += parseInt(input[i + 1]);
+        } else if (input[i] === 'up') {
+            aim -= parseInt(input[i + 1]);
+        }        
+        i++;
+    }
+    console.log('Your horizontal position is ' + horizontal);
+    console.log('Your depth is ' + depth);
+    final = horizontal * depth;
+    console.log('Your final position is ' + final);
+    return;
+}
+
+console.log('=======================================');
+console.log('Diving Part 1');
+console.log('---------------------------------------');
 dive();
+console.log('=======================================');
+console.log('Diving Part 2: Improved Calculations')
+console.log('---------------------------------------');
+dive2();
+console.log('=======================================');
