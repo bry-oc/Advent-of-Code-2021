2cd9f4b8b92f78efe8e0137e6a4710f8093d7a46
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-12-02 21:10:18 -0500

    completed day 1 part 2

--- day1.ts
+++ day1.ts
@@ -16,6 +16,8 @@ function sonarSweep(){
         } else if (parseInt(input[i]) > parseInt(input[i - 1])){
             increased++;
             message = '(increased)';
+        } else if (parseInt(input[i]) == parseInt(input[i - 1])) {
+            message = '(no change)';
         }
         console.log(input[i], message);
     }
@@ -24,5 +26,35 @@ function sonarSweep(){
 }
 
 sonarSweep();
+
+function sonarThreeSweep() {
+    let input = fs.readFileSync('./day1.txt', 'utf-8').trim().split('\n');
+    let increased = 0;
+    let currentSum;
+    let previousSum;
+    for (let i = 0; i < input.length - 2; i++) {
+        let message;   
+        if(i != 0) {
+            previousSum = parseInt(input[i-1]) + parseInt(input[i]) + parseInt(input[i + 1]);
+        }    
+        currentSum = parseInt(input[i]) + parseInt(input[i+1]) + parseInt(input[i+2]);
+        if (previousSum == undefined) { 
+            message = '(N/A - no previous sum)';
+        } else if (currentSum < previousSum) {
+            message = '(decreased)';
+        } else if (currentSum > previousSum) {
+            increased++;
+            message = '(increased)';
+        } else if (currentSum == previousSum) {
+            message = '(no change)';
+        }
+
+        console.log(currentSum, message);
+    }
+    console.log('There are ' + increased + ' sums that are larger than the previous measurement.');
+    return;
+}
+
+sonarThreeSweep();
 
 
